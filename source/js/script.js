830f66d6c773682682460dd0b6a3bcf5bcf8beb7
VSArkhipov <savikaripov48@gmail.com>
2018-02-17 02:44:36 +0300

    добавляет автоматизацию

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,12 @@ var link = document.querySelector(".main-nav__item--button");
 var popup = document.querySelector(".main-nav__list--closed");
 var button = document.querySelector(".main-nav__toggle--closed");
 
-var modallink = document.querySelector(".week__cost")
-var modalbutton = document.querySelector(".week__order--closed");
+var modallink = document.querySelector(".listen")
+var modalbutton = document.querySelector(".modalbutton-closed");
 var modalwindow = document.querySelector(".modal__closed");
 
 var modalclose = document.querySelector(".modal__close")
 var modaltoggle = document.querySelector(".modal__close")
-
-var goodlink = document.querySelector(".good__description")
-var goodbutton = document.querySelector(".good__basket--closed");
-var goodwindow = document.querySelector(".modal__closed");
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -24,9 +20,9 @@ link.addEventListener("click", function (evt) {
 modallink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalwindow.classList.toggle("modal__opened");
-  modalbutton.classList.toggle("week__order--opened");
+  modalbutton.classList.toggle("modalbutton-opened");
   modalwindow.classList.toggle("modal__closed");
-  modalbutton.classList.toggle("week__order--closed");
+  modalbutton.classList.toggle("modalbutton-closed");
 });
 
 modalclose.addEventListener("click", function (evt) {
@@ -35,12 +31,4 @@ modalclose.addEventListener("click", function (evt) {
   modalwindow.classList.toggle("modal__closed");
   modaltoggle.classList.toggle("modal__close");
   modalwindow.classList.toggle("modal__opened");
-});
-
-goodlink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  goodwindow.classList.toggle("modal__opened");
-  goodbutton.classList.toggle("good__basket--opened");
-  goodwindow.classList.toggle("modal__closed");
-  goodbutton.classList.toggle("good__basket--closed");
 });
