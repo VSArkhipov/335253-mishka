e90c4a24e567720196554a5de680f342a739c564
VSArkhipov <savikaripov48@gmail.com>
2018-02-18 20:56:41 +0300

    исправление после первой попытки

--- source/js/script.js
+++ source/js/script.js
@@ -1,34 +1,29 @@
 var link = document.querySelector(".main-nav__item--button");
-var popup = document.querySelector(".main-nav__list--closed");
-var button = document.querySelector(".main-nav__toggle--closed");
-
-var modallink = document.querySelector(".listen")
-var modalbutton = document.querySelector(".modalbutton-closed");
-var modalwindow = document.querySelector(".modal__closed");
-
-var modalclose = document.querySelector(".modal__close")
-var modaltoggle = document.querySelector(".modal__close")
-
+var popup = document.querySelector(".main-nav__list--opened");
+var button = document.querySelector(".main-nav__toggle--opened");
+var modal__link = document.querySelector(".listen")
+var modal__button = document.querySelector(".modalbutton-closed");
+var modal__window = document.querySelector(".modal__closed");
+var modal__close = document.querySelector(".modal__close")
+var modal__toggle = document.querySelector(".modal__close")
 link.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popup.classList.toggle("main-nav__list--opened");
-  button.classList.toggle("main-nav__toggle--opened");
-	popup.classList.toggle("main-nav__list--closed");
+  evt.preventDefault();
+  popup.classList.toggle("main-nav__list--closed");
   button.classList.toggle("main-nav__toggle--closed");
+  popup.classList.toggle("main-nav__list--opened");
+  button.classList.toggle("main-nav__toggle--opened");
 });
-
-modallink.addEventListener("click", function (evt) {
+modal__link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalwindow.classList.toggle("modal__opened");
-  modalbutton.classList.toggle("modalbutton-opened");
-  modalwindow.classList.toggle("modal__closed");
-  modalbutton.classList.toggle("modalbutton-closed");
+  modal__window.classList.toggle("modal__opened");
+  modal__button.classList.toggle("modalbutton-opened");
+  modal__window.classList.toggle("modal__closed");
+  modal__button.classList.toggle("modalbutton-closed");
 });
-
-modalclose.addEventListener("click", function (evt) {
+modal__close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modaltoggle.classList.toggle("modal__close");
-  modalwindow.classList.toggle("modal__closed");
-  modaltoggle.classList.toggle("modal__close");
-  modalwindow.classList.toggle("modal__opened");
+  modal__toggle.classList.toggle("modal__close");
+  modal__window.classList.toggle("modal__closed");
+  modal__toggle.classList.toggle("modal__close");
+  modal__window.classList.toggle("modal__opened");
 });
