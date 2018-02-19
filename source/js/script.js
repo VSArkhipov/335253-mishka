8b5d68c5b8178ccd8c697eef5203ab2ecd7ec43b
VSArkhipov <savikaripov48@gmail.com>
2018-02-20 02:18:45 +0300

    исправления после первой попытки #3

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,14 @@
+var js = document.querySelector(".js-no");
 var link = document.querySelector(".main-nav__item--button");
 var popup = document.querySelector(".main-nav__list--opened");
 var button = document.querySelector(".main-nav__toggle--opened");
-var modal__link = document.querySelector(".listen")
-var modal__button = document.querySelector(".modalbutton-closed");
+var modal__link = document.querySelector(".listen");
+var modal__button = document.querySelector(".modal__button--closed");
 var modal__window = document.querySelector(".modal__closed");
-var modal__close = document.querySelector(".modal__close")
-var modal__toggle = document.querySelector(".modal__close")
+var modal__background = document.querySelector(".modal__background--closed");
+var modal__close = document.querySelector(".modal__close");
+var modal__toggle = document.querySelector(".modal__close");
+js.classList.remove("js-no");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("main-nav__list--closed");
@@ -16,14 +19,18 @@ link.addEventListener("click", function (evt) {
 modal__link.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal__window.classList.toggle("modal__opened");
-  modal__button.classList.toggle("modalbutton-opened");
+  modal__button.classList.toggle("modal__button--opened");
+  modal__background.classList.toggle("modal__background--opened");
   modal__window.classList.toggle("modal__closed");
-  modal__button.classList.toggle("modalbutton-closed");
+  modal__button.classList.toggle("modal__button--closed");
+  modal__background.classList.toggle("modal__background--closed");
 });
 modal__close.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal__toggle.classList.toggle("modal__close");
   modal__window.classList.toggle("modal__closed");
+  modal__background.classList.toggle("modal__background--closed");
   modal__toggle.classList.toggle("modal__close");
   modal__window.classList.toggle("modal__opened");
+  modal__background.classList.toggle("modal__background--opened");
 });
